4c8a08432d1101a2086e5e815cd7dc158e8d8488
Angelo Zuniga <angeloeboy10@gmail.com>
2021-01-28 12:53:30 +0800

    Added animations

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 
+
+
 $(".navbars").click(() => {
 
     if($(".links").hasClass("clicked")){
@@ -11,7 +13,57 @@ $(".navbars").click(() => {
         $(".links").addClass("clicked");
     }
 
-
-
-
 })
+
+;(function($, win) {
+    $.fn.inViewport = function(cb) {
+       return this.each(function(i,el) {
+         function visPx(){
+           var elH = $(el).outerHeight(),
+               H = $(win).height(),
+               r = el.getBoundingClientRect(), t=r.top, b=r.bottom;
+           return cb.call(el, Math.max(0, t>0? Math.min(elH, H-t) : Math.min(b, H)));  
+         }
+         visPx();
+         $(win).on("resize scroll", visPx);
+       });
+    };
+  }(jQuery, window));
+
+
+
+  $(".text-container").inViewport(function(px) {
+    // console.log( px ); // `px` represents the amount of visible height
+    if(px > 0) {
+      // do this if element enters the viewport // px > 0
+      $(this).addClass("animate__animated animate__fadeInRightBig");
+    }
+  }); // Here you can chain other jQuery methods to your selector
+
+
+
+  $(".item-containers").inViewport((px) => {
+    if(px > 0){
+        let i = 1;
+        
+        let addAnimation = () =>{
+            setTimeout(() => {
+                $(".item-containers .item:nth-child(" + i + ")").addClass("animate__animated animate__zoomIn animate__faster").css("opacity", "1")
+                console.log(".item-containers .item:nth-child(" + i + ")")
+                i++;
+                if(i < 6){
+                    addAnimation();
+                }
+            }, 200)
+        }
+
+        addAnimation();
+        
+
+
+      console.log("entered")
+    }
+  });
+
+ 
+
